2d090df631aa3269fc09c23b309bb8d6bd6919c1
Dmitriy Khovrich <dkhovrich@readdle.com>
2021-04-22 14:45:16 +0300

    filter-type.ts

---
+++ src/easy/filter-type.ts
@@ -0,0 +1,9 @@
+type FilterKeysByValue<Obj, T> = {
+    [K in keyof Obj]: Obj[K] extends T ? K : never
+}[keyof Obj]
+
+type MyType = {a: 1, b: number, c: string};
+
+type NumberKeys = FilterKeysByValue<MyType, number>;
+
+type NumberValues = Pick<MyType, NumberKeys>;
